bc4bc89d284f67e3a0dcd2b810ab1f69b810d206
Christian Peraza <christianperaza2000@gmail.com>
2025-02-13 08:41:41 -0300

    Add and style main list items

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,141 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
 
 const Main = () => {
   return (
     <main className='main'>
-      <section className='main__popularArtistsSection'>
-        <h2>Popular artists</h2>
-        <a href='#'>
-          <p>Show all</p>
-        </a>
-      </section>
+      <div className='item-section'>
+        <header className='item-section__header'>
+          <h2>Popular artists</h2>
+          <a href='#'>
+            <p>Show all</p>
+          </a>
+        </header>
+        <ul className='item-section__list'>
+          <li className='item-section__item'>
+            <a href='#'>
+              <div className='item-section__img-container'>
+                <img
+                  src='https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4'
+                  alt='Henrique & Juliano image artist'
+                />
+                <FontAwesomeIcon icon={faCirclePlay} />
+              </div>
+              <section className='item-section__texts'>
+                <h3>Henrique & Juliano</h3>
+                <p>Artist</p>
+              </section>
+            </a>
+          </li>
+          <li className='item-section__item'>
+            <a href='#'>
+              <div className='item-section__img-container'>
+                <img
+                  src='https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4'
+                  alt='Henrique & Juliano image artist'
+                />
+                <FontAwesomeIcon icon={faCirclePlay} />
+              </div>
+              <section className='item-section__texts'>
+                <h3>Henrique & Juliano</h3>
+                <p>Artist</p>
+              </section>
+            </a>
+          </li>
+          <li className='item-section__item'>
+            <a href='#'>
+              <div className='item-section__img-container'>
+                <img
+                  src='https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4'
+                  alt='Henrique & Juliano image artist'
+                />
+                <FontAwesomeIcon icon={faCirclePlay} />
+              </div>
+              <section className='item-section__texts'>
+                <h3>Henrique & Juliano</h3>
+                <p>Artist</p>
+              </section>
+            </a>
+          </li>
+          <li className='item-section__item'>
+            <a href='#'>
+              <div className='item-section__img-container'>
+                <img
+                  src='https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4'
+                  alt='Henrique & Juliano image artist'
+                />
+                <FontAwesomeIcon icon={faCirclePlay} />
+              </div>
+              <section className='item-section__texts'>
+                <h3>Henrique & Juliano</h3>
+                <p>Artist</p>
+              </section>
+            </a>
+          </li>
+          <li className='item-section__item'>
+            <a href='#'>
+              <div className='item-section__img-container'>
+                <img
+                  src='https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4'
+                  alt='Henrique & Juliano image artist'
+                />
+                <FontAwesomeIcon icon={faCirclePlay} />
+              </div>
+              <section className='item-section__texts'>
+                <h3>Henrique & Juliano</h3>
+                <p>Artist</p>
+              </section>
+            </a>
+          </li>
+          <li className='item-section__item'>
+            <a href='#'>
+              <div className='item-section__img-container'>
+                <img
+                  src='https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4'
+                  alt='Henrique & Juliano image artist'
+                />
+                <FontAwesomeIcon icon={faCirclePlay} />
+              </div>
+              <section className='item-section__texts'>
+                <h3>Henrique & Juliano</h3>
+                <p>Artist</p>
+              </section>
+            </a>
+          </li>
+          <li className='item-section__item'>
+            <a href='#'>
+              <div className='item-section__img-container'>
+                <img
+                  src='https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4'
+                  alt='Henrique & Juliano image artist'
+                />
+                <FontAwesomeIcon icon={faCirclePlay} />
+              </div>
+              <section className='item-section__texts'>
+                <h3>Henrique & Juliano</h3>
+                <p>Artist</p>
+              </section>
+            </a>
+          </li>
+          <li className='item-section__item'>
+            <a href='#'>
+              <div className='item-section__img-container'>
+                <img
+                  src='https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4'
+                  alt='Henrique & Juliano image artist'
+                />
+                <FontAwesomeIcon icon={faCirclePlay} />
+              </div>
+              <section className='item-section__texts'>
+                <h3>Henrique & Juliano</h3>
+                <p>Artist</p>
+              </section>
+            </a>
+          </li>
+        </ul>
+      </div>
     </main>
   );
 };
