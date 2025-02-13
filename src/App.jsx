4b1757b39b25b3e0f332c8e0846e82a48523d0e9
Christian Peraza <christianperaza2000@gmail.com>
2025-02-13 06:18:43 -0300

    Add main and style first elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Header from './components/Header';
+import Main from './components/Main';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 };
---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Main = () => {
+  return (
+    <main className='main'>
+      <section className='main__popularArtistsSection'>
+        <h2>Popular artists</h2>
+        <a href='#'>
+          <p>Show all</p>
+        </a>
+      </section>
+    </main>
+  );
+};
+
+export default Main;
