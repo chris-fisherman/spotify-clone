18526e492a2361e5e68cbc86d9d121055be1a200
Christian Peraza <christianperaza2000@gmail.com>
2025-02-14 08:14:24 -0300

    Change song img border

--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -6,11 +6,16 @@ import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 const SingleItem = ({ id, image, name, banner, artist, idPath }) => {
+  const imgClassName = idPath === '/song' ? 'square-border-img' : <></>;
   return (
     <li className='item-section__single-item'>
       <Link to={`${idPath}/${id}`}>
         <div className='item-section__img-container'>
-          <img src={image} alt={`${name} image artist`} />
+          <img
+            src={image}
+            alt={`${name} image artist`}
+            className={imgClassName}
+          />
           <FontAwesomeIcon icon={faCirclePlay} />
         </div>
         <section className='item-section__texts'>
