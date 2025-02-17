7133e407a740741f57a4bd4e7b0e4fd731dd0193
Christian Peraza <christianperaza2000@gmail.com>
2025-02-17 17:19:46 -0300

    Add and style song header

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,8 +1,97 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import AsideLibrary from '../components/AsideLibrary';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+// const Header = styled.header`
+//   &:before {
+//     content: '';
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//     right: 0;
+//     bottom: 0;
+//     background: url('https://i.scdn.co/image/ab67616d00001e0299c540c0f78793fcea84a920');
+//     background-size: cover;
+//     background-position: center;
+//     filter: blur(50px);
+//     transform: scale(3);
+//     z-index: -1;
+//   }
+// `;
+
+const ImgShadow = styled.div`
+  &:after {
+    content: '';
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-image: url('https://i.scdn.co/image/ab67616d00001e0299c540c0f78793fcea84a920');
+    position: absolute;
+    background-size: contain;
+    background-repeat: no-repeat;
+    filter: drop-shadow(0px 0px) blur(40px);
+    z-index: -1;
+  }
+`;
+
+const HeaderBackground = styled.div`
+  &:before {
+    content: '';
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-image: url('https://i.scdn.co/image/ab67616d00001e0299c540c0f78793fcea84a920');
+    position: absolute;
+    background-size: cover;
+    background-repeat: no-repeat;
+    filter: drop-shadow(0px 0px) blur(40px);
+    z-index: -2;
+  }
+`;
 
 const Song = () => {
-  return <div>Song</div>;
+  return (
+    <main>
+      <AsideLibrary />
+      <div className='song-main'>
+        <header className='song-header'>
+          <div className='song-header__img-container'>
+            <img
+              src={
+                'https://i.scdn.co/image/ab67616d00001e0299c540c0f78793fcea84a920'
+              }
+              alt='Image'
+            />
+            <ImgShadow />
+          </div>
+          <section className='song-header__texts-container'>
+            <p>Song</p>
+            <h2>De Trás Pra Frente - Ao Vivo</h2>
+            <div className='texts-container__below-texts'>
+              <img
+                src={
+                  'https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4'
+                }
+                alt='Image'
+              />
+              <Link to={'/artist/1'}>Henrique & Juliano</Link>
+              <span>•</span>
+              <p>De Trás Pra Frente - Ao Vivo</p>
+              <span>•</span>
+              <p className='texts-container__below-texts--gray'>2:43</p>
+            </div>
+          </section>
+          <HeaderBackground />
+        </header>
+      </div>
+    </main>
+  );
 };
 
 export default Song;
